44981c7163ba51d9393e1c14edbb2cf8788e1291
arpan bhattacharya <arpan.bhattacharya@skilljar.com>
2022-02-28 09:59:21 -0500

    remove dependencies from root dir

--- lambda-function-github-webhook/lambda_function.py
+++ lambda-function-github-webhook/lambda_function.py
@@ -17,11 +17,15 @@ def verify_signature(headers, body):
     else:
         return compare_digest(received, expected)
 
-def list_github_repos():
-    github_access_token = os.environ.get("GITHUB_ACCESS_TOKEN").encode("utf-8")
+def comment_on_pr(PR_NUMBER, comment):
+    github_access_token = str(os.environ.get("GITHUB_ACCESS_TOKEN"))
     g = Github(github_access_token)
-    for repo in g.get_user().get_repos():
-        print(repo.name)
+    #for repo in g.get_user().get_repos():
+    #    print(repo.name)
+    repo_name = 'arpan-skilljar/testrepo'
+    repo = g.get_repo(repo_name)
+    pr = repo.get_pull(PR_NUMBER)
+    pr.create_issue_comment(comment)
 
 def lambda_handler(event, context):
     print('inside lambda execution...')
@@ -29,47 +33,53 @@ def lambda_handler(event, context):
         print('verified signature success')
     else: 
         print('signature failed')
-    #print("Received event: " + json.dumps(event, indent=2))
+        return {
+            'statusCode': 400,
+            'headers': {'Content-Type': 'text'},
+            'body': 'this request is not coming from github',
+        }
 
     body = event.get('body', "")
     github_event = event.get('multiValueHeaders').get('X-GitHub-Event')
     github_event = str(github_event[0])
     print('the github event is: ' + github_event)
     body = urllib.parse.unquote_plus(body)
-    
-    #print('body of request is..')
-    #print(body)
+
+    # get rid of 'payload='
     body = body[8:]
-    #print("body after substring: " + body)
-    body_json = json.loads(body)
+
+    # convert to json object
+    body = json.loads(body)
     
-    action = body_json.get('action')
+    action = body.get('action')
     print("the action is: " + action)
     
     if github_event == "pull_request":
-
-        pull_request_num = str(body_json.get('number'))
-        pull_request_title = body_json.get('pull_request').get('title')
-        
+        pull_request_num = str(body.get('number'))
+        pull_request_title = body.get('pull_request').get('title')
         print("the pull request number is: " + pull_request_num)
         print('the pull request title is: ' + pull_request_title)
         
     elif github_event == "issue_comment":
-        
-        pull_request_num = str(body_json.get('issue').get('number'))
-        issue_comment = body_json.get('comment').get('body')
-        print("the pull request number is: " + pull_request_num)
+        pull_request_num = body.get('issue').get('number')
+        issue_comment = body.get('comment').get('body')
+        print("the pull request number is: " + str(pull_request_num))
         print('the comment on the pull request is: ' + issue_comment)
-    
-    list_github_repos()
+        print('the issue comment truncated is: ' + issue_comment[0:11])
+        from_reviewapp = issue_comment[0:11]
+        if from_reviewapp == "[ReviewApp]":
+            return {
+                'statusCode': 200,
+                'headers': {'Content-Type': 'text'},
+                'body': 'nothing to do, comment from reviewapp',
+            }
+        elif issue_comment == "start review app": 
+            comment_on_pr(pull_request_num, "[ReviewApp] whatever comment we want")
+            
+    #list_github_repos()
 
     return {
         'statusCode': 200,
         'headers': {'Content-Type': 'application/json'},
         'body': json.dumps(event),
     }
-    #elif route == "/":
-    #    print('inside /webhook')
-    #    print(json.dumps(event))
-    #    # TODO Write webhook receiver code
-    #    pass
