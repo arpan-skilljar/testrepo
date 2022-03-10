6caae96ce8ee55acd2e7df9fbd4d835a36c39a1f
arpan bhattacharya <arpan.bhattacharya@skilljar.com>
2022-03-10 11:57:57 -0500

    add function to store payload in dynamo

--- ReviewApp-Lambda-Broker/lambda_function.py
+++ ReviewApp-Lambda-Broker/lambda_function.py
@@ -8,14 +8,31 @@ from hmac import HMAC, compare_digest
 from github import Github
 from botocore.exceptions import ClientError
 import logging
+import time
 
+# configure logger
 log_level = os.environ['LOG_LEVEL']
 logger = logging.getLogger()
-
 if log_level == "info":
     logger.setLevel(logging.INFO)
 else:
     logger.setLevel(logging.ERROR)
+
+# configure secrets manager client
+secret = boto3.client('secretsmanager')
+
+# configure SNS client
+TOPIC_ARN = os.environ['TOPIC_ARN']
+sns_resource = boto3.resource('sns')
+topic = sns_resource.Topic(TOPIC_ARN)
+
+# configure DynamoDB client
+dynamodb = boto3.client('dynamodb')
+table_name = os.environ['REVIEW_APP_TABLE']
+
+# configure epoch time
+epoch_time = str(round(time.time()))
+logger.info('got epoch time ' + epoch_time)
 
 def publish_message(topic, message, attributes):
     try:
@@ -37,11 +54,8 @@ def publish_message(topic, message, attributes):
         return message_id
 
 def get_secret_value(name, stage=None):
-    secret = boto3.client('secretsmanager')
-
     if name is None:
         raise ValueError
-
     try:
         kwargs = {'SecretId': name}
         if stage is not None:
@@ -71,9 +85,21 @@ def comment_on_pr(PR_NUMBER, comment, repo_name):
     repo = g.get_repo(repo_name)
     pr = repo.get_pull(PR_NUMBER)
     pr.create_issue_comment(comment)
+ 
+def add_github_payload_to_dynamo(table_name, attributes):
+    dynamodb.put_item(
+    TableName = table_name, Item = {
+      'payload_uuid':{'S' : attributes['uid']},
+      'github_payload':{'S' : "testing..."},
+      'pr_num':{'N' : attributes["pr_number"]},
+      'commit_utc_sha':{'S' : attributes['commit_utc_sha']}
+      })
 
 def lambda_handler(event, context):
     logger.info('inside lambda execution...')
+
+    #logger.info('Event: %s' % json.dumps(event))
+
     if verify_signature(event["headers"], event["body"]):
         logger.info('verified signature success')
     else: 
@@ -86,6 +112,10 @@ def lambda_handler(event, context):
 
     body = event.get('body', "")
     github_event = event.get('multiValueHeaders').get('X-GitHub-Event')
+
+    # get UID from github payload
+    uid = event.get('multiValueHeaders').get('X-GitHub-Delivery')
+
     github_event = str(github_event[0])
     logger.info('the github event is: ' + github_event)
     body = urllib.parse.unquote_plus(body)
@@ -100,12 +130,6 @@ def lambda_handler(event, context):
 
     action = body.get('action')
     logger.info("the action is: " + action)
-
-    # setup SNS
-    #TOPIC_ARN = 'arn:aws:sns:us-west-2:420762066547:githubLambdaBroker'
-    TOPIC_ARN = os.environ['TOPIC_ARN']
-    sns_resource = boto3.resource('sns')
-    topic = sns_resource.Topic(TOPIC_ARN)
     
     if github_event == "pull_request":
         pull_request_num = body.get('number')
@@ -113,16 +137,26 @@ def lambda_handler(event, context):
         pull_request_creator = body.get('pull_request').get('user').get('login')
         pull_request_branch = body.get('pull_request').get('head').get('ref')
         pull_request_commit = body.get('pull_request').get('head').get('sha')
-        pull_request_repo = body.get('pull_request').get('repository').get('full_name')        
+        pull_request_repo = body.get('repository').get('full_name')        
         logger.info('the pull request number is: ' + str(pull_request_num))
         logger.info('the pull request title is: ' + pull_request_title)
         logger.info('the pull request creator is: ' + pull_request_creator)
         logger.info('the pull request branch is: ' + pull_request_branch)        
         logger.info('the pull request commit is: ' + pull_request_commit)
-        logger.info('the pull request repo is: ' + pull_request_repo)        
+        logger.info('the pull request repo is: ' + pull_request_repo)    
 
+        commit_utc_sha = epoch_time + '_' + pull_request_commit
 
-    # what about action == reopened when PR is reopened
+        attributes = {'commit_utc_sha': commit_utc_sha, \
+            'uid': uid, \
+            'pr_number': str(pull_request_num), \
+            'commit_sha': pull_request_commit, \
+            'creator': pull_request_creator, \
+            'pull_request_branch': pull_request_branch,   \
+            'pull_request_title': pull_request_title}
+
+        logger.info('adding github payload to dynamo...')
+        add_github_payload_to_dynamo(table_name, attributes)
 
     if (github_event == "pull_request") and (action == "opened" or action == "reopened"):
         logger.info('Posted to github PR based on Git PR open event...')
@@ -130,9 +164,7 @@ def lambda_handler(event, context):
 
         # SNS publish
         message = 'github-reviewapp-actions'
-        attributes = {'action': 'open', 'pr_number': str(pull_request_num), \
-            'commit_sha': pull_request_commit, \
-            'creator': pull_request_creator}
+        attributes["action"] = "open"
         publish_message(topic, message, attributes)
 
     if (github_event == "pull_request") and (action == "synchronize"):
@@ -142,9 +174,7 @@ def lambda_handler(event, context):
 
         # SNS publish
         message = 'github-reviewapp-actions'
-        attributes = {'action': 'updated', 'pr_number': str(pull_request_num), \
-            'commit_sha': pull_request_commit, \
-            'creator': pull_request_creator}
+        attributes["action"] = "updated"
         publish_message(topic, message, attributes)        
 
     if (github_event == "pull_request") and (action == "closed"):
@@ -153,9 +183,7 @@ def lambda_handler(event, context):
 
         # SNS publish
         message = 'github-reviewapp-actions'
-        attributes = {'action': 'closed', 'pr_number': str(pull_request_num), \
-            'commit_sha': pull_request_commit, \
-            'creator': pull_request_creator}
+        attributes["action"] = "closed"
         publish_message(topic, message, attributes)   
 
     return {
